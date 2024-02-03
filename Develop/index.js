337ac3b6317f4a7eb2a58a2ecace474ce39f5f7a
Robert Solorzano <robertsolorzano510@gmail.com>
2024-02-03 12:35:11 -0800

    imported generateMarkdown into index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,12 +10,13 @@ const questions = [
       name: 'title',
       message: 'What is the name of this project?',
     }
+    //need to add more questions later
 ];
-
 
 inquirer.prompt(questions)
   .then((responses) => {
     console.log('User Responses: ', responses);
+    writeToFile();//need to make this function and arguments
   })
   .catch((error) => {
     console.error('An error occured:', error);
