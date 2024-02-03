98306109dbc9ecf3d64ed932173cf82ef6e5922e
Robert Solorzano <robertsolorzano510@gmail.com>
2024-02-03 15:06:23 -0800

    added conditional in renderLicenseSection calling the previous function for the link and appending into markdown language underneath

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -27,7 +27,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'None') {
+      return '';
+  }
+  const licenseLink = renderLicenseLink(license); //get the license link
+  return `
+## License
+
+This project is licensed under the ${license} license. For more information, see [here](${licenseLink}).
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
