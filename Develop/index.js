cc098991b7dc36f413fedc011b41f7ab57ea19ea
Robert Solorzano <robertsolorzano510@gmail.com>
2024-02-03 12:10:19 -0800

    basic prompt setup, currently only logging reponses to test if working

--- Develop/index.js
+++ Develop/index.js
@@ -3,8 +3,21 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of this project?',
+    }
 ];
+
+
+inquirer.prompt(questions)
+  .then((responses) => {
+    console.log('User Responses: ', responses);
+  })
+  .catch((error) => {
+    console.error('An error occured:', error);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
